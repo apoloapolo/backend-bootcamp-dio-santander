965c680001093c11cf695e932fee8caf9afef3fc
Vinicius Albuquerque (Apolo) <vinicius.cius@gmail.com>
2022-12-09 15:57:26 -0300

    Adicionado projeto criado durante as aulas

---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,10 @@
+package com.project.bootcamp.util;
+
+public abstract class MessageUtils {
+
+    public static final String STOCK_ALREDY_EXISTS = "Stock alredy exists in the database";
+    public static final String NO_RECORDS_FOUND = "No records found";
+
+
+
+}
